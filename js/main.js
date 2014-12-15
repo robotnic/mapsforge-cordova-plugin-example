76f45f3427afee7614a5107b764da5693cdbfdc0
bernhard <bz@datenkueche.com>
2014-12-15 01:53:41 +0100

    js-beautify

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mapfile="/mnt/sdcard/Download/austria.map"; //  <-------------- change this
+var mapfile = "/mnt/sdcard/Download/austria.map"; //  <-------------- change this
 
 var isCordovaApp = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
 var map = L.map('map').setView([48.205, 16.3], 13);
@@ -6,50 +6,53 @@ var map = L.map('map').setView([48.205, 16.3], 13);
 
 // define the tile layers
 
-var layers={
-   osm:L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+var layers = {
+    osm: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-   }),
-   cycle:L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+    }),
+    cycle: L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
         attribution: '&copy;  <a href="http://www.thunderforest.com/">OpenCycleMap</a> <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-   }),
-   offline:L.offlineTileLayer({
-       maxZoom: 18,
-       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-   })
+    }),
+    offline: L.offlineTileLayer({
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    })
 };
 
 //add osm layer to map
-if(!isCordovaApp){
-layers.osm.addTo(map);
-                L.control.layers({
-                    "OpenStreetMap":layers.osm,
-                    "Cycle":layers.cycle,
-                }).addTo(map);
+if (!isCordovaApp) {
+    layers.osm.addTo(map);
+    L.control.layers({
+        "OpenStreetMap": layers.osm,
+        "Cycle": layers.cycle,
+    }).addTo(map);
 }
 
 document.addEventListener("deviceready", function() {
     console.log("deviceready");
-    if(typeof(mapsforge)!=="undefined"){
-        mapsforge.cache.initialize(mapfile, 
+    if (typeof(mapsforge) !== "undefined") {
+        mapsforge.cache.setThemePath("Elevelo_Cycling.xml", function() {
+            console.log("theme ok");
+        }, function(error) {
+            console.log("theme error: " + error);
+        });
+        mapsforge.cache.initialize(mapfile,
             function() {
                 console.log("map file loaded");
                 layers.offline.addTo(map);
 
                 //the layer button top right 
                 L.control.layers({
-                    "OpenStreetMap":layers.osm,
-                    "Cycle":layers.cycle,
-                    "Offline":layers.offline 
+                    "OpenStreetMap": layers.osm,
+                    "Cycle": layers.cycle,
+                    "Offline": layers.offline
                 }).addTo(map);
 
             },
             function(error) {
-                console.log("error loading map file: "+mapfile);
+                console.log("error loading map file: " + mapfile);
             });
-    }else{
+    } else {
         console.log("mapsforge not loaded");
     }
 }, false);
-
-
